62ef5c2b2cb208063f8483a5d456c1d05f922249
Hugo BRETECHE <hubretec@e2r3p18.clusters.42paris.fr>
2022-03-29 14:57:38 +0200

    added mutex for death checking

--- src/philo.c
+++ src/philo.c
@@ -6,7 +6,7 @@
 /*   By: hubretec <hubretec@student.42.fr >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 11:51:12 by hubretec          #+#    #+#             */
-/*   Updated: 2022/03/25 14:24:50 by hubretec         ###   ########.fr       */
+/*   Updated: 2022/03/29 14:56:33 by hubretec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,12 +38,24 @@ int	check_eat(t_philo *philos)
 	return (1);
 }
 
+int	check_death(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->env->death);
+	if (philo->env->died)
+	{
+		pthread_mutex_unlock(&philo->env->death);
+		return (1);
+	}
+	pthread_mutex_unlock(&philo->env->death);
+	return (0);
+}
+
 void	*live(void *ptr)
 {
 	t_philo	*philo;
 
 	philo = (t_philo *)ptr;
-	while (!philo->env->died)
+	while (!check_death(philo))
 	{
 		philo_eat(philo);
 		philo_sleep_think(philo, SLEEP);
@@ -59,25 +71,4 @@ void	launch(t_philo *philos)
 	i = -1;
 	while (++i < philos->env->nb_philos)
 		pthread_create(&philos[i].thread, NULL, live, &philos[i]);
-	while (1)
-	{
-		if (check_eat(philos))
-		{
-			free_philo(philos);
-			exit(EXIT_SUCCESS);
-		}
-		pthread_mutex_lock(&philos->env->death);
-		if (philos->env->died)
-		{
-			printf("%u %d died\n", (get_ms() - philos->env->start_time),
-				philos->env->died);
-			free_philo(philos);
-			pthread_mutex_unlock(&philos->env->death);
-			pthread_mutex_destroy(&philos->env->death);
-			free(philos->env);
-			free(philos);
-			exit(EXIT_SUCCESS);
-		}
-		pthread_mutex_unlock(&philos->env->death);
-	}
 }
